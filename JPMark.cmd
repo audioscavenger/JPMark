b17d7ca18047798f416d0adb4ed2a1867af55b04
audioscavenger <dev@derewonko.com>
2021-12-20 00:30:12 -0700

    release 1.0.0 - includes jpegtran - extract with imagick as png - insert text with transparency as jpg - drop inside original file with jpegtran + optimize Huffman
    Signed-off-by: audioscavenger <dev@derewonko.com>

--- JPMark.cmd
+++ JPMark.cmd
@@ -1,43 +1,68 @@
 @echo OFF
 pushd %~dp0
-
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: * requisites:
+:: - jpegtran https://jpegclub.org/jpegtran/                            Jpeg lossless operations
+:: - iMagick  https://www.imagemagick.org/script/download.php#windows   Portable Win64 static at 16 bits-per-pixel component.
+::
 :: the sample image is 6048x4024 JPEG, quality: 91, subsampling ON (2x1)
 :: we want the watermark to be centered at roughly the bottom with:
 :: wwidth   = width * 40 / 100 modulo 8
 :: wheight  = height * 7 / 100 modulo 8
 :: wleft    = (width - wwidth) /2 modulo 8
 :: wtop     = (height - wheight) + 80 modulo 8
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :init
-set "PATH=%PATH%;E:\PortableApps\Magick"
-set DEBUG=true
-set Scale=100
-set extract=extract.png
-set watermark=watermark.png
-set alpha=0.3
-set fontColor=255,255,255
+set author=AudioscavengeR
+set authorEmail=dev@derewonko.com
+set version=1.0.0
 
-:: Point_Size and wwidth really depends on the font type and the height of the watermark; play with textScale to see if it fits
-set textScale=80
-set text=^&ric photography
+:: codepage 65001 is pretty much UTF8
+chcp 65001 >NUL
+set DEBUG=
+set extract=extract.png
+set watermark=watermark.jpg
+
+:defaults
+set Scale=100
+set alpha=0.2
+set fontColor=255,255,255
+:: 99.99% of jpeg have a 8x8 block DCT with 2x1 sample factor, but this will be extracted from the original jpeg anyway
+set hsample=16
+set vsample=8
+
+::::::::::::::::::::::::::::::::::::::::::::: customize your own values here :::::::::::::::::::::::::::::::::::::::::::::
+:custom
+:: it is critical that you provide here the path to jpegtran and imagick here
+set "PATH=%PATH%;E:\PortableApps\Magick;E:\wintools\multimedia\jpegtran"
+set text=©^&ric photography
 set font=Romantica-RpXpW.ttf
+:: watermark width and height as a percentage of your pictures
 set wwidthPct=30
 set wheightPct=9
+:: Point_Size and wwidth really depends on the font type and the height of the watermark; play with textScale to see if it fits; the example Romantica-RpXpW.ttf provided is a bit special
+set textScale=80
+:: output filename, cannot be the same as the original file
+set outputFile=%~dpn1-C%~x1
+::::::::::::::::::::::::::::::::::::::::::::: customize your own values here :::::::::::::::::::::::::::::::::::::::::::::
 
 :prechecks
 call :set_colors
-del /f /q %extract% %watermark%
+del /f /q %extract% %watermark% 2>NUL
 
 :main
+echo Processing %~nx1 ...
 
-call :getSIZE %1
+call :getJpegInfo %1
 call :getWSIZE
 call :calculatePoint_Size
 
-REM magick -define jpeg:size=%SIZE% -extract 2480x280+1740+3664  %1 %extract%
-magick -define jpeg:size=%SIZE% -extract %WSIZE%+%WPOS%  %1 %extract%
-
-call :watermark %extract% %watermark%
+:: we need a png extract for transparency, bro
+call :extractMagick %1 %extract%
+REM call :extractJpegtran %1 %extract%
+call :genWatermark %extract% %watermark%
+call :pasteWatermark %watermark% %1 %outputFile%
 
 goto :end
 
@@ -48,30 +73,35 @@ set /A wwidth   = width * wwidthPct / 100
 set /A wheight  = height * wheightPct / 100
 call :logDEBUG WSIZE    =%wwidth%x%wheight%
 
-set /A wwidth   = wwidth - (wwidth %% 8)
-set /A wheight  = wheight - (wheight %% 8)
+set /A wwidth   = wwidth - (wwidth %% hsample)
+set /A wheight  = wheight - (wheight %% vsample)
 
 set /A wleft    = (width - wwidth) / 2
 set /A wtop     = (height - wheight) - 80
+set /A wleft    = wleft - (wleft %% hsample)
+set /A wtop     = wtop - (wtop %% vsample)
 
 call :logDEBUG WSIZE  m8=%wwidth%x%wheight%
-call :logDEBUG WPOS     =%wleft%x%wtop%
+call :logDEBUG WPOS     =%wleft%+%wtop%
 
 set WSIZE=%wwidth%x%wheight%
 set "WPOS=%wleft%+%wtop%"
 goto :EOF
 
 
-:getSIZE
+:getJpegInfo
+:: https://imagemagick.org/script/identify.php
+:: https://imagemagick.org/script/escape.php
+:: magick identify Filename[frame #] image-format widthxheight page-widthxpage-height+x-offset+y-offset colorspace user-time elapsed-time
 :: IT WORKS!!!! but you have to define the size of the layers. Both commands are equivalent:
-rem FOR /F "tokens=* USEBACKQ" %%s IN (`magick getSIZE -format "%%[fx:w]x%%[fx:h]" %1`) DO SET SIZE=%%s
-FOR /F "tokens=* USEBACKQ" %%s IN (`magick convert  -ping %1 -format "%%wx%%h" info:`) DO SET SIZE=%%s
-echo SIZE=%SIZE%
+rem FOR /F "tokens=1,2 USEBACKQ" %%a IN (`magick identify -format "%%[fx:w]x%%[fx:h] %%[jpeg:sampling-factor]" %1`) DO SET SIZE=%%a %%b
+rem FOR /F "tokens=1,2 USEBACKQ" %%a IN (`magick identify -format ""%%wx%%h %[jpeg:sampling-factor]" %1`) DO SET SIZE=%%a %%b
+FOR /F "tokens=1-4 USEBACKQ" %%a IN (`magick identify -format "%%[fx:w] %%[fx:h] %%[jpeg:sampling-factor] %%Q" %1`) DO set "width=%%a" & set "height=%%b" & set "sampling=%%c" & set "Quality=%%d"
 
-for /f "tokens=1,2" %%a in ("%SIZE:x= %") DO (
-  set width=%%a
-  set height=%%b
-)
+set SIZE=%width%x%height%
+for /f "tokens=1"   %%a in ("%sampling:,= %") DO set "samplingFactor=%%a"
+for /f "tokens=1,2" %%a in ("%samplingFactor:x= %") DO set /A "hsample=%%a * 8" & set /A "vsample=%%b * 8"
+
 call :logDEBUG SIZE=%SIZE%
 goto :EOF
 
@@ -88,21 +118,45 @@ IF %Scale% NEQ 100 (
 
 goto :EOF
 
-:watermark input output
+:extractMagick input output
+magick -define jpeg:size=%SIZE% -extract %WSIZE%+%WPOS% %1 %2
+goto :EOF
+
+:genWatermark input output
+set JPEG_OPTIONS=-sampling-factor %samplingFactor% -quality %Quality%
 
 magick convert %1 %OPTIONS% ^
 %resize% ^
 -gravity Center ^( -size %WSIZE% xc:none -font %font% -pointsize %Point_Size% -fill rgba(%fontColor%,%alpha%) -strokewidth 7 -annotate 0 "%text%" -blur 0x1 ^) ^
 -composite -font %font% -pointsize %Point_Size% -fill rgba(%fontColor%,1) -stroke none      -annotate 0 "%text%" ^
-%QUALITY% ^
+%JPEG_OPTIONS% ^
 %2
-
-REM -gravity %gTOP% -size %SIZE% xc:none -font Impact -pointsize %scaledPoint_Size% -stroke rgba(0,0,0,1) -strokewidth 7 -annotate 0 "%annotateTOP%" -blur 0x1  ^
-REM -gravity Center -size %WSIZE% xc:none -font %font% -pointsize %Point_Size% -fill rgba(%fontColor%,%alpha%) -stroke none -annotate 0 "%text%" -blur 0x1 ^
-REM -composite -font %font% -pointsize %Point_Size% -fill rgba(%fontColor%,1) -stroke none      -annotate 0 "%text%" ^
 
 goto :EOF
 
+:extractJpegtran input output
+:: https://stackoverflow.com/questions/37560777/how-can-i-losslessly-crop-a-jpeg-in-r/37612027
+REM The -crop switch specifies the rectangular subarea WxH+X+Y, 
+REM and -optimize is an option for reducing file size without quality loss by optimizing the Huffman table. 
+REM For a complete list of switches see jpegtran -help.
+:: jpegtran jpegjoin https://jpegclub.org/jpegtran/
+:: jpegcrop http://sylvana.net/jpegcrop/
+:: lossless croping: http://www.ben.com/jpeg/
+
+REM DEBUG: set SIZE=6048x4024
+REM DEBUG: set WSIZE=1814x362
+REM DEBUG: set WSIZE=1808x360
+REM DEBUG: set WPOS=2120+3584
+
+REM jpegtran -crop %newWidth%x%newHeight%+0+0 -optimize %1 %2
+REM jpegtran -crop %WSIZE%+%WPOS% DZ6_6045.JPG extract.jpg
+jpegtran -crop %WSIZE%+%WPOS% -optimize %1 %2
+
+goto :EOF
+
+:pasteWatermark watermark input output
+jpegtran -drop +%WPOS% %1 -optimize %2 %3
+goto :EOF
 
 
 :logDEBUG
@@ -145,6 +199,6 @@ goto :EOF
 
 
 :end
-%watermark%
+IF DEFINED DEBUG %outputFile%
 REM timeout /t 5
 
